4898e30d28e5bb50fd295714c3e3b26aaa18da1e
sivaram <athotasivaram@gmail.com>
2023-06-23 12:53:19 +0530

    siv

--- terraform/session4/Provider.tf
+++ terraform/session4/Provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.64.0"
     }
   }
+  backend "s3" {
+    bucket = "timeing-remote-state"
+    key    = "session-04"
+    region = "ap-south-1"
+    dynamodb_table = "timeing-lock"
+  }
 }
 
 provider "aws" {
